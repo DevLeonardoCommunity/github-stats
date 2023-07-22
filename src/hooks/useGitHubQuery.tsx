930f3a36fde6710da65e71d4ef2f030a9e34725f
Leonardo Montini <lion.48m@gmail.com>
2023-07-22 21:19:37 +0200

    Query GitHub with GraphQL

---
+++ src/hooks/useGitHubQuery.tsx
@@ -0,0 +1,58 @@
+import type { GraphQlQueryResponseData } from "@octokit/graphql";
+import { useSession } from "next-auth/react";
+import { Octokit } from "octokit";
+import { useEffect, useState } from "react";
+
+export const useGitHubQuery = (): any => {
+  const { data: session }: any = useSession();
+
+  const [data, setData] = useState<any>();
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (!session?.user) return;
+
+    const getData = async () => {
+      setIsLoading(true);
+      const gh = new Octokit({
+        auth: session.accessToken,
+      });
+
+      const { user: result } = await gh.graphql<GraphQlQueryResponseData>({
+        query: `query ($login: String!) {
+            user(login: $login) {
+              login
+              avatarUrl
+              contributionsCollection {
+                commitContributionsByRepository(maxRepositories: 50) {
+                  repository {
+                    name
+                  }
+                  contributions(orderBy: {field: COMMIT_COUNT, direction: DESC}) {
+                    totalCount
+                  }
+                }
+                issueContributionsByRepository(maxRepositories: 50) {
+                  repository {
+                    name
+                  }
+                  contributions(orderBy: {direction: DESC}) {
+                    totalCount
+                  }
+                }
+              }
+            }
+          }
+          `,
+        login: session.user.login,
+      });
+
+      setData(result);
+      setIsLoading(false);
+    };
+
+    getData();
+  }, [session]);
+
+  return { data, isLoading };
+};
--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,13 +1,19 @@
+import { useGitHubQuery } from "@/hooks/useGitHubQuery";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function Test() {
   const { data: session } = useSession();
+
+  const { data: ghStats, isLoading } = useGitHubQuery();
 
   if (session) {
     return (
       <>
         Signed in as {session.user?.name} <br />
         <button onClick={() => signOut()}>Sign out</button>
+        <pre>
+          {isLoading ? "Loading... " : JSON.stringify(ghStats, null, 2)}
+        </pre>
       </>
     );
   }
