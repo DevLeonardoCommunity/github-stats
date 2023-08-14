d4c2ec8e310f380b24452ca695bd5d6a86f1189c
Leonardo Montini <lion.48m@gmail.com>
2023-08-14 16:38:09 +0200

    Allow login with credentials using a github token

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import { GitHubUser } from "@/types/session";
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { Octokit } from "octokit";
 
 export default NextAuth({
   providers: [
@@ -16,14 +18,61 @@ export default NextAuth({
         } as GitHubUser;
       },
     }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {},
+      async authorize() {
+        if (!process.env.DEV_GITHUB_TOKEN)
+          throw new Error("No DEV_GITHUB_TOKEN env variable set");
+
+        if (process.env.NODE_ENV !== "development")
+          throw new Error("CredentialsProvider can only be used in dev mode");
+
+        const gh = new Octokit({
+          auth: process.env.DEV_GITHUB_TOKEN,
+        });
+
+        const { viewer } = await gh.graphql<{ viewer: any }>(`
+          {
+            viewer {
+              id
+              name
+              login
+              avatarUrl
+            }
+          }
+          `);
+
+        return {
+          id: viewer.id,
+          name: viewer.name,
+          login: viewer.login,
+          image: viewer.avatarUrl,
+          accessToken: process.env.DEV_GITHUB_TOKEN,
+        } as GitHubUser & { accessToken: string };
+      },
+    }),
   ],
   callbacks: {
     jwt({ token, user, account }) {
+      // Coming from GithubProvider
       if (account && user) {
         return {
           accessToken: account.access_token,
           refreshToken: account.refresh_token,
           user,
+        };
+      }
+
+      // Coming from CredentialsProvider only used in dev mode
+      if (token) {
+        const {
+          user: { accessToken, ...user },
+        } = token as any;
+
+        return {
+          accessToken,
+          user: user,
         };
       }
 
@@ -34,7 +83,6 @@ export default NextAuth({
         ...session,
         user: token.user,
         accessToken: token.accessToken,
-        mario: 1,
       };
     },
   },
