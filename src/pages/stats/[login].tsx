3f52fc2f4bb0a17f82bc6814584949acffc4d451
piyushjha0409 <peeyush0409@gmail.com>
2023-08-18 19:30:15 +0530

    FIX #3 final changes applied

--- src/pages/stats/[login].tsx
+++ src/pages/stats/[login].tsx
@@ -2,7 +2,7 @@ import { RepositoryContributionsCard } from "@/components";
 import { useGitHubPullRequests } from "@/hooks";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
-import {useState } from "react";
+import { useState } from "react";
 
 const yearsRange = 4;
 
@@ -19,75 +19,68 @@ export default function Stats() {
     login as string
   );
 
-
   //function for exporting the json format data
   const exportJSON = () => {
-
     const jsonStringData = JSON.stringify(repositories, null, 2);
- 
-    const blob = new Blob([jsonStringData], {type: 'application/json'});
+
+    const blob = new Blob([jsonStringData], { type: "application/json" });
 
     const url = URL.createObjectURL(blob);
 
-    const link = document.createElement('a')
+    const link = document.createElement("a");
     //setting the link as url of the blob
     link.href = url;
-    link.download = 'data.json';
+    link.download = "data.json";
     link.click();
     URL.revokeObjectURL(url);
   };
 
-
   //function for exporting the data format text
 
-  const exportText  = () => {
-      
+  const exportText = () => {
     const text = generateText();
-    const blob = new Blob([text], {type: 'text/plain'});
+    const blob = new Blob([text], { type: "text/plain" });
 
     const url = URL.createObjectURL(blob);
 
-    const link = document.createElement('a')
+    const link = document.createElement("a");
     //setting the link as url of the blob
     link.href = url;
-    link.download = 'data.txt';
+    link.download = "data.txt";
     link.click();
     URL.revokeObjectURL(url);
-  }
+  };
 
   //function for generating the text
-function generateText() {
-    
+  function generateText() {
     let text = "List of repositories and their pull requests:\n\n";
-    
+
     for (const repoData of repositories) {
-        const repositoryName = repoData.repository.name;
-        const ownerLogin = repoData.repository.owner.login;
-        const stargazerCount = repoData.repository.stargazerCount;
-        const avatarUrl = repoData.repository.owner.avatarUrl;
-        
-        text += `Repository: ${repositoryName}\n`;
-        text += `Owner: ${ownerLogin}\n`;
-        text += `Stargazers: ${stargazerCount}\n`;
-        text += `Owner Avatar: ${avatarUrl}\n\n`;
-        
-        const contributions = repoData.contributions.nodes;
-        text += "Contributions:\n";
-        for (const contribution of contributions) {
-            const prId = contribution.pullRequest.id;
-            const prTitle = contribution.pullRequest.title;
-            const prState = contribution.pullRequest.state;
-            text += `- Pull Request: ${prTitle}\n`;
-            text += `  ID: ${prId}\n`;
-            text += `  State: ${prState}\n`;
-        }
-        
-        text += "\n";
+      const repositoryName = repoData.repository.name;
+      const ownerLogin = repoData.repository.owner.login;
+      const stargazerCount = repoData.repository.stargazerCount;
+      const avatarUrl = repoData.repository.owner.avatarUrl;
+
+      text += `Repository: ${repositoryName}\n`;
+      text += `Owner: ${ownerLogin}\n`;
+      text += `Stargazers: ${stargazerCount}\n`;
+      text += `Owner Avatar: ${avatarUrl}\n\n`;
+
+      const contributions = repoData.contributions.nodes;
+      text += "Contributions:\n";
+      for (const contribution of contributions) {
+        const prId = contribution.pullRequest.id;
+        const prTitle = contribution.pullRequest.title;
+        const prState = contribution.pullRequest.state;
+        text += `- Pull Request: ${prTitle}\n`;
+        text += `  ID: ${prId}\n`;
+        text += `  State: ${prState}\n`;
+      }
+
+      text += "\n";
     }
     return text;
-}
-
-
+  }
 
   return (
     <div className="h-full w-full px-4 flex flex-col gap-4">
@@ -153,25 +146,33 @@ function generateText() {
         <div className="w-full grid xl:grid-cols-3 gap-3 mb-3 md:grid-cols-2">
           {repositories?.map(({ repository, contributions }) => (
             <RepositoryContributionsCard
-            key={repository.name}
-            repository={repository}
-            contributions={contributions}
+              key={repository.name}
+              repository={repository}
+              contributions={contributions}
             />
-            ))}
-          </div>
+          ))}
+        </div>
       ) : format === "text" ? (
         <pre>
-         <button className="bg-blue-500 p-2 m-1 rounded hover:bg-blue-900" onClick={exportText}>Export as Text</button>
-         {generateText()}
+          <button
+            className="bg-blue-500 p-2 m-1 rounded hover:bg-blue-900"
+            onClick={exportText}
+          >
+            Export as Text
+          </button>
+          {generateText()}
         </pre>
       ) : (
         <div>
-        <button className="bg-blue-500 p-2 m-1 rounded hover:bg-blue-900" onClick={exportJSON}>Export as JSON</button>
-        <pre>
-        {JSON.stringify(repositories, null, 2)}  
-        </pre>
+          <button
+            className="bg-blue-500 p-2 m-1 rounded hover:bg-blue-900"
+            onClick={exportJSON}
+          >
+            Export as JSON
+          </button>
+          <pre>{JSON.stringify(repositories, null, 2)}</pre>
         </div>
-        )}
+      )}
     </div>
   );
 }
