8d8afe634dd0de6168b0a6494b18d65a559c608e
Jakub Fronczyk <jakubfronczyk@gmail.com>
2023-08-22 16:42:49 +0200

    change button placement

--- src/pages/stats/[login].tsx
+++ src/pages/stats/[login].tsx
@@ -139,15 +139,15 @@ export default function Stats() {
         );
       case "text":
         return (
-          <pre>
+          <div>
             <button
               className="bg-blue-500 p-2 m-1 rounded hover:bg-blue-900"
               onClick={exportText}
             >
               Export as Text
             </button>
-            {generateText()}
-          </pre>
+            <pre>{generateText()}</pre>
+          </div>
         );
       default:
         return (
