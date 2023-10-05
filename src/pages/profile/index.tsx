31e617a769738b57eabdd49bc097a9e3d1b9f85a
thititongumpun <thiti180536@gmail.com>
2023-10-05 15:53:55 +0700

    feat: profile page github activity graph

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -6,16 +6,42 @@ import { useGitHubQuery } from "@/hooks";
 import Image from "next/image";
 import Link from "next/link";
 import { exportAsImage } from "@/utils";
+import GitHubCalendar from "react-github-calendar";
+import { useState } from "react";
+
+interface Activity {
+  date: string;
+  count: number;
+  level: 0 | 1 | 2 | 3 | 4;
+}
 
 export default function Profile() {
   const { data } = useGitHubQuery<UserProfile>(userProfileQuery);
+  const [checked, setChecked] = useState<boolean>(false);
 
   if (!data) return "Loading...";
 
+  const selectLastHalfYear = (contributions: Activity[]) => {
+    const currentYear = new Date().getFullYear();
+    const currentMonth = new Date().getMonth();
+    const shownMonths = 6;
+
+    return contributions.filter((activity: Activity) => {
+      const date = new Date(activity.date);
+      const monthOfDay = date.getMonth();
+
+      return (
+        date.getFullYear() === currentYear &&
+        monthOfDay > currentMonth - shownMonths &&
+        monthOfDay <= currentMonth
+      );
+    });
+  };
+
   return (
     <div>
-      <div className="flex justify-center">
-        <div className="dropdown pt-10 ">
+      <div className="flex flex-col justify-center items-center py-10">
+        <div className="dropdown ">
           <button
             tabIndex={0}
             className="btn btn-primary p-2 m-1 rounded cursor-pointer"
@@ -45,6 +71,17 @@ export default function Profile() {
               </button>
             </li>
           </ul>
+        </div>
+        <div className="flex mx-auto">
+          <label className="cursor-pointer label gap-2">
+            <span className="label badge badge-primary">Activity Calendar</span>
+            <input
+              type="checkbox"
+              className="toggle toggle-primary"
+              checked={checked}
+              onChange={(e) => setChecked(e.target.checked)}
+            />
+          </label>
         </div>
       </div>
       <div>
@@ -81,6 +118,20 @@ export default function Profile() {
                   GitHub
                 </Link>
               </div>
+              {checked && (
+                <GitHubCalendar
+                  username={data.user.login}
+                  transformData={selectLastHalfYear}
+                  blockSize={10}
+                  fontSize={10}
+                  loading={!data}
+                  colorScheme="light"
+                  hideColorLegend
+                  labels={{
+                    totalCount: "{{count}} contributions in the last half year",
+                  }}
+                />
+              )}
             </div>
           </div>
         </div>
