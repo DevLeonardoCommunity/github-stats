08e2254c2d11f2fd5c479277b85fb924e5772fef
sa.cux <sabrina.cuccureddu@green-share.it>
2024-01-30 10:17:32 +0100

    add TS type

--- src/components/RepositoryContributionsCard.tsx
+++ src/components/RepositoryContributionsCard.tsx
@@ -1,4 +1,4 @@
-import { Contributions, Repository } from "@/types/github";
+import { Contributions, PullRequestNode, Repository } from "@/types/github";
 import Image from "next/image";
 import Link from "next/link";
 import { FaCodeMerge } from "react-icons/fa6";
@@ -52,35 +52,40 @@ export const RepositoryContributionsCard = ({
             </div>
           </div>
           <div className="max-h-[22rem] hide-scrollbar overflow-auto flex flex-col gap-1">
-            {nodes?.map(({ pullRequest: { state, title, id, url } }: any) => (
-              <div key={id} className="flex items-center justify-between gap-2">
-                <a
-                  href={url}
-                  target="_blank"
-                  className="truncate"
-                  title={title}
+            {nodes?.map(
+              ({ pullRequest: { state, title, id, url } }: PullRequestNode) => (
+                <div
+                  key={id}
+                  className="flex items-center justify-between gap-2"
                 >
-                  {title}
-                </a>
-                <span
-                  className={`h-fit rounded p-1 ${
-                    state === "MERGED"
-                      ? "bg-purple-500"
-                      : state === "CLOSED"
-                      ? "bg-red-500"
-                      : "bg-green-500"
-                  }`}
-                >
-                  {state === "MERGED" ? (
-                    <FaCodeMerge size={18} />
-                  ) : state === "CLOSED" ? (
-                    <IoIosCloseCircleOutline size={18} />
-                  ) : (
-                    <GoIssueOpened size={18} />
-                  )}
-                </span>
-              </div>
-            ))}
+                  <a
+                    href={url}
+                    target="_blank"
+                    className="truncate"
+                    title={title}
+                  >
+                    {title}
+                  </a>
+                  <span
+                    className={`h-fit rounded p-1 ${
+                      state === "MERGED"
+                        ? "bg-purple-500"
+                        : state === "CLOSED"
+                        ? "bg-red-500"
+                        : "bg-green-500"
+                    }`}
+                  >
+                    {state === "MERGED" ? (
+                      <FaCodeMerge size={18} />
+                    ) : state === "CLOSED" ? (
+                      <IoIosCloseCircleOutline size={18} />
+                    ) : (
+                      <GoIssueOpened size={18} />
+                    )}
+                  </span>
+                </div>
+              )
+            )}
           </div>
         </div>
       </div>
