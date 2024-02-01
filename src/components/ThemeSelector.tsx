6b7acc99ff7411efbf11d4ea54c73cf16a41b3c4
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2024-02-01 12:38:37 -0500

    fix: making the webpage more accessible (#105)
    * fix: change primary color to accessible equivalent
    
    * fix: add for attribute for theme selector label
    
    * feat: add aria-label in label for dropdown menu
    
    * fix: add aria-label
    
    * fix: add for attribute
    
    * Revert "fix: add aria-label"
    
    This reverts commit 6c5bf835ebb7cb56fa492e6c77f3176b3299a8a5.

--- src/components/ThemeSelector.tsx
+++ src/components/ThemeSelector.tsx
@@ -31,6 +31,7 @@ export function ThemeSelector() {
     <>
       <div className="dropdown dropdown-bottom dropdown-end">
         <label
+          for="themeToggle"
           tabIndex={0}
           className="btn btn-circle btn-ghost m-1"
           data-testid="themeSelectorButton"
