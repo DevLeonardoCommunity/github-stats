73d7de251e8ada1912c07dab50cada0f9505c5c5
sa.cux <sabrina.cuccureddu@green-share.it>
2024-02-22 10:29:28 +0100

    removed unused component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,7 +1,6 @@
 export * from "./ExportDropdownButton";
 export * from "./Dropdown";
 export * from "./ReposFilters";
-export * from "./ExportDropdown";
 export * from "./FormatStatsRender";
 export * from "./RepositoryContributionsCard";
 export * from "./CardSkeleton";
