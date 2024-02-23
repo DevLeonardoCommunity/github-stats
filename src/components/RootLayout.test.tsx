0af843acc6c1642743f6008b99c242a919e73098
sa.cux <sabrina.cuccureddu@green-share.it>
2024-02-23 19:35:03 +0100

    Revert "fix imports to be consistent"
    This reverts commit dd2c0b1609d36cdaa23c33b78a9295e36567ee4e.

--- src/components/RootLayout.test.tsx
+++ src/components/RootLayout.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from "@testing-library/react";
-import { RootLayout } from "@/components";
+import { RootLayout } from "./RootLayout";
 import { vi, describe, test, expect } from "vitest";
 
 vi.mock("./Header", () => ({
--- src/components/ThemeSelector.test.tsx
+++ src/components/ThemeSelector.test.tsx
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, test, expect } from "vitest";
-import { ThemeSelector } from "@/components";
+import { ThemeSelector } from "./ThemeSelector";
 
 describe("ThemeSelector", () => {
   test("should change light Icon if click Dark mode", () => {
